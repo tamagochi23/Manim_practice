62c63541f07157c51aec8560ebbc0965445a971a
DavidMonsalves98 <davidmonsalves98@gmail.com>
2025-08-26 22:01:49 -0400

    Move MovingAngle Scene to update folder

--- Tutorial/Updater_shapes/intro_manim_updaters.py
+++ Tutorial/Updater_shapes/intro_manim_updaters.py
@@ -3,56 +3,41 @@ class MovingAngle(Scene):
     def construct(self):
         rotation_center = LEFT
 
-        theta_tracker = ValueTracker(72)
-        line1 = Line(LEFT,RIGHT)
-        line_moving  = Line(LEFT, RIGHT).set_color(BLUE)
-        line_ref = line_moving.copy().set_color(BLUE)
+        theta_tracker = ValueTracker(110)
+        line1 = Line(LEFT, RIGHT)
+        line_moving = Line(LEFT, RIGHT)
+        line_ref = line_moving.copy()
         line_moving.rotate(
-            theta_tracker.get_value() * DEGREES,
-            about_point=rotation_center
+            theta_tracker.get_value() * DEGREES, about_point=rotation_center
         )
-        a = Angle(
-            line1, line_moving, radius=0.5,
-            other_angle=False
-        ).set_color(RED)
-
+        a = Angle(line1, line_moving, radius=0.5, other_angle=False)
         tex = MathTex(r"\theta").move_to(
             Angle(
-                line1, line_moving, radius=0.5 + 3 * SMALL_BUFF,
-                other_angle=False
+                line1, line_moving, radius=0.5 + 3 * SMALL_BUFF, other_angle=False
             ).point_from_proportion(0.5)
         )
 
-        self.add(line1, line_moving,a, tex)
-        self.wait(1)
+        self.add(line1, line_moving, a, tex)
+        self.wait()
 
         line_moving.add_updater(
-            lambda m: m.become(line_ref.copy()).rotate( 
-                theta_tracker.get_value() * DEGREES,
-                about_point=rotation_center
+            lambda x: x.become(line_ref.copy()).rotate(
+                theta_tracker.get_value() * DEGREES, about_point=rotation_center
             )
         )
 
         a.add_updater(
-            lambda m: m.become(
-                Angle(
-                    line1, line_moving, radius=0.5,
-                    other_angle=False
-                ).set_color(RED)
-            )
+            lambda x: x.become(Angle(line1, line_moving, radius=0.5, other_angle=False))
         )
-
         tex.add_updater(
-            lambda m: m.move_to(
+            lambda x: x.move_to(
                 Angle(
-                    line1, line_moving, radius=0.5 + 3 * SMALL_BUFF,
-                    other_angle=False
+                    line1, line_moving, radius=0.5 + 3 * SMALL_BUFF, other_angle=False
                 ).point_from_proportion(0.5)
             )
         )
 
         self.play(theta_tracker.animate.set_value(40))
         self.play(theta_tracker.animate.increment_value(140))
-        self.play(theta_tracker.animate.set_value(355))
-
-
+        self.play(tex.animate.set_color(RED), run_time=0.5)
+        self.play(theta_tracker.animate.set_value(350))
