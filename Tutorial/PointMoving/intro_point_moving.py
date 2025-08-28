d00fe4887d63c8ee35d6add4e02df8f3bf5ce011
DavidMonsalves98 <davidmonsalves98@gmail.com>
2025-08-27 22:59:24 -0400

    Moving Points and Line

--- Tutorial/PointMoving/intro_point_moving.py
+++ Tutorial/PointMoving/intro_point_moving.py
@@ -1,4 +1,5 @@
 from manim import *
+import random
 
 class PointMovingOnShapes(Scene):
     def construct(self):
@@ -18,3 +19,29 @@ class PointMovingOnShapes(Scene):
         self.play(MoveAlongPath(dot, square), run_time=3, rate_func=linear)
         self.play(Transform(dot, dot4))
         self.play(MoveAlongPath(dot, triangle), run_time=3, rate_func=linear)
+        self.wait()
+        
+class PointsAndLine(Scene):
+    def construct(self):
+        d1,d2 = Dot(color=BLUE), Dot(color = GREEN)
+        circle = Circle(radius=3, color=WHITE).move_to(ORIGIN)
+        dg = VGroup(d1,d2).arrange(RIGHT)
+        l1 = Line(d1.get_center(), d2.get_center()).set_color(RED)
+        x = ValueTracker(0)
+        y = ValueTracker(0)
+        d1.add_updater(lambda m: m.set_x(x.get_value()))
+        d2.add_updater(lambda m: m.set_y(y.get_value()))
+        l1.add_updater(lambda m: m.become(Line(d1.get_center(), d2.get_center())))
+        self.add(d1,d2,l1)
+        i = 0
+        while i < 5:
+            if i % 2 == 0:
+                value_x = -1
+                value_y = 1
+            else:
+                value_x = 1
+                value_y = -1
+            self.play(x.animate.set_value(3* value_x), y.animate.set_value(3*value_y), rate_func=linear, run_time=0.7)
+            self.play(GrowFromCenter(circle))
+            i += 1
+
