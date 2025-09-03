c61a7a9beb00741a5e801822393190e5cfb38b0f
DavidMonsalves98 <davidmonsalves98@gmail.com>
2025-09-02 23:09:26 -0400

    TwoGraphs: Create function and its derivative, two graphs on screen.

--- Tutorial/Graphs/intro_graphs.py
+++ Tutorial/Graphs/intro_graphs.py
@@ -66,5 +66,71 @@ class GraphExample(Scene):
         self.play(t.animate.set_value(2), run_time=3, rate_func=linear)
         self.wait(1)
 
+class TwoGraphs(Scene):
+    def construct(self):
+
+        def func1(x):
+            return x**2
+
+        def func2(x):
+            return 2*x
+
+        eq1 = MathTex("f(x) = x^2").set_color(ORANGE).scale(1)
+        eq2 = MathTex("f'(x) = 2x").set_color(ORANGE).scale(1)
+
+        ax1 = NumberPlane((-3,3), (-4,4)).add_coordinates().scale(0.8)
+        ax2 = NumberPlane((-3,3), (-4,4)).add_coordinates().scale(0.8)
+
+        a = VGroup(eq1, ax1).arrange(DOWN, buff=0.1)
+        b = VGroup(eq2, ax2).arrange(DOWN, buff=0.1)
+
+        VGroup(a, b).arrange()        
+        graph1 = ax1.plot(func1, color=GREEN)
+        graph2 = ax2.plot(func2, color=GREEN)
+        dot1 = Dot(point=ax1.c2p(-2, func1(-2)), color=RED)
+        dot2 = Dot(point=ax2.c2p(-2, func2(-2)), color=RED)
 
 
+        t = ValueTracker(-2)
+
+        dot1.add_updater(lambda m: m.move_to(ax1.c2p(t.get_value(), func1(t.get_value()))))
+        dot2.add_updater(lambda m: m.move_to(ax2.c2p(t.get_value(), func2(t.get_value()))))
+
+        line_1 = ax1.get_vertical_line(ax1.input_to_graph_point(-2, graph1), color = RED, stroke_width=5)
+        line_2 = ax1.get_horizontal_line(ax1.input_to_graph_point(-2, graph1), color = RED, stroke_width=5)
+
+        line_1.add_updater(lambda m: m.become(ax1.get_vertical_line(ax1.input_to_graph_point(t.get_value(), graph1), color = RED, stroke_width=5)))
+        line_2.add_updater(lambda m: m.become(ax1.get_horizontal_line(ax1.input_to_graph_point(t.get_value(), graph1), color = RED, stroke_width=5)))
+
+        line_3 = ax2.get_vertical_line(ax2.input_to_graph_point(-2, graph2), color = RED, stroke_width=5)
+        line_4 = ax2.get_horizontal_line(ax2.input_to_graph_point(-2, graph2), color = RED, stroke_width=5)
+
+        line_3.add_updater(lambda m: m.become(ax2.get_vertical_line(ax2.input_to_graph_point(t.get_value(), graph2), color = RED, stroke_width=5)))
+        line_4.add_updater(lambda m: m.become(ax2.get_horizontal_line(ax2.input_to_graph_point(t.get_value(), graph2), color = RED, stroke_width=5)))
+
+        self.add(ax1, ax2, eq1, eq2, graph1, graph2)
+        self.add(line_1, line_2, line_3, line_4)
+        self.play(Create(dot1), Create(dot2))
+        self.wait(0.5)
+        self.play(t.animate.set_value(2), run_time=10, rate_func=linear)
+
+
+class Love_graph(Scene):
+
+    def func(self,t):
+        return (16*np.sin(t)**3,
+        13*np.cos(t),
+        0)
+
+    def construct(self):
+        axes = Axes(x_range=[-16, 16],
+                    y_range=[-15, 15],
+                    axis_config={"color": BLUE},
+                    x_length = 8,
+                    y_length = 6
+                    )
+        func = ParametricFunction(self.func, t_range = (0,TAU), color=RED)
+
+        self.play(Create(axes))
+        self.add(func.scale(2))
+        self.wait(1)
