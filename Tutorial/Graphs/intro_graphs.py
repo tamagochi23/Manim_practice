40ecc50949c84901e2e967204ccf80e4696b9b67
DavidMonsalves98 <davidmonsalves98@gmail.com>
2025-09-22 22:01:15 -0300

    f(x) = x^3 analysis

--- Tutorial/Graphs/intro_graphs.py
+++ Tutorial/Graphs/intro_graphs.py
@@ -134,3 +134,52 @@ class Love_graph(Scene):
         self.play(Create(axes))
         self.add(func.scale(2))
         self.wait(1)
+
+
+class TwoGraphsVertical(Scene):
+    def construct(self):
+
+            def func1(x):
+                return x**3
+
+            def func2(x):
+                return 3*x**2
+
+            eq1 = MathTex("f(x) = x^3").set_color(ORANGE).scale(1)
+            eq2 = MathTex("f'(x) = 3x^2").set_color(ORANGE).scale(1)
+
+            ax1 = Axes(x_range=[-3, 3], y_range=[-4, 4], axis_config={"color": BLUE}, x_length=8, y_length=6, tips = False).add_coordinates().scale(0.8)
+            ax2 = Axes(x_range=[-3, 3], y_range=[-4, 4], axis_config={"color": BLUE}, x_length=8, y_length=6, tips = False).add_coordinates().scale(0.8)
+
+            a = VGroup(eq1, ax1).arrange(DOWN, buff=0.1)
+            b = VGroup(eq2, ax2).arrange(DOWN, buff=0.1)
+
+            VGroup(a, b).arrange()
+            graph1 = ax1.plot(func1, color=GREEN, x_range=[-2, 2])
+            graph2 = ax2.plot(func2, color=GREEN, x_range=[-2, 2])
+            dot1 = Dot(point=ax1.c2p(-1.5, func1(-1.5)), color=RED)
+            dot2 = Dot(point=ax2.c2p(-1.5, func2(-1.5)), color=RED)
+
+
+            t = ValueTracker(-1.5)
+
+            dot1.add_updater(lambda m: m.move_to(ax1.c2p(t.get_value(), func1(t.get_value()))))
+            dot2.add_updater(lambda m: m.move_to(ax2.c2p(t.get_value(), func2(t.get_value()))))
+
+            line_1 = ax1.get_vertical_line(ax1.input_to_graph_point(-1.5, graph1), color = RED, stroke_width=5)
+            line_2 = ax1.get_horizontal_line(ax1.input_to_graph_point(-1.5, graph1), color = RED, stroke_width=5)
+
+            line_1.add_updater(lambda m: m.become(ax1.get_vertical_line(ax1.input_to_graph_point(t.get_value(), graph1), color = RED, stroke_width=5)))
+            line_2.add_updater(lambda m: m.become(ax1.get_horizontal_line(ax1.input_to_graph_point(t.get_value(), graph1), color = RED, stroke_width=5)))
+
+            line_3 = ax2.get_vertical_line(ax2.input_to_graph_point(-1.5, graph2), color = RED, stroke_width=5)
+            line_4 = ax2.get_horizontal_line(ax2.input_to_graph_point(-1.5, graph2), color = RED, stroke_width=5)
+
+            line_3.add_updater(lambda m: m.become(ax2.get_vertical_line(ax2.input_to_graph_point(t.get_value(), graph2), color = RED, stroke_width=5)))
+            line_4.add_updater(lambda m: m.become(ax2.get_horizontal_line(ax2.input_to_graph_point(t.get_value(), graph2), color = RED, stroke_width=5)))
+
+            self.add(ax1, ax2, eq1, eq2, graph1, graph2)
+            self.add(line_1, line_2, line_3, line_4)
+            self.play(Create(dot1), Create(dot2))
+            self.wait(0.5)
+            self.play(t.animate.set_value(1.5), run_time=15, rate_func=linear)
