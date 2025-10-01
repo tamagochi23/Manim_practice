4f0cb1e816826c9d2014a4a05821b6b0ada02595
DavidMonsalves98 <davidmonsalves98@gmail.com>
2025-09-30 23:25:56 -0300

    updater texts

--- Tutorial/Riemann/riemann_rectangles.py
+++ Tutorial/Riemann/riemann_rectangles.py
@@ -1,6 +1,15 @@
 from manim import *
 
 class GetRiemannRectangles(Scene):
+
+    def calculate_riemann_sum(self, func, a, b, dx):
+        n = int(np.floor((b-a)/dx))
+        total_sum = 0
+        for i in range(n):
+            x_i = a + i*dx
+            total_sum += func(x_i) * dx
+        return total_sum
+
     def construct(self):
         ax = Axes(
                 y_range = [-1,1],
@@ -11,44 +20,51 @@ class GetRiemannRectangles(Scene):
             ).scale(0.8)
         graph = ax.plot(lambda x: np.sin(x), x_range = [0,2*PI], color = BLUE)
 
+        i = 1
+        dx = 0.6
+        n = int(np.floor(PI/dx))
 
-        title = MathTex(r"\int_0^{\pi} \sin(x) \, dx = 2", font_size = 48).to_edge(UP)
 
-        sum_riemann_text = MathTex(r"\sum_{i=1}^{n} f(x_i^*) \Delta x", f"= ", font_size = 48).to_edge(DOWN)
+        title = MathTex(r"\int_0^{\pi} \sin(x) \, dx = 2", font_size = 48).to_edge(UP+LEFT*5)
 
+        sum_riemann_value = self.calculate_riemann_sum(lambda x: np.sin(x), 0, PI, dx)
+
+        sum_riemann_text = MathTex(r"\sum_{i=1}^{",f"{n}", "}", "\sin(x_i)", f"{round(dx,2):.2f}", f"={round(sum_riemann_value,3)}", font_size = 48).to_edge(UP+RIGHT*5)
+
+        origin_label = MathTex("0", font_size = 48).next_to(ax.c2p(0,0), DOWN*4.5+RIGHT*0.5)
+        pi_label = MathTex(r"\pi", font_size = 48).next_to(ax.c2p(PI,0), DOWN*4.5+LEFT*0.5)
         a = VGroup(ax,graph).to_edge(DOWN)
-        b = VGroup(title,sum_riemann_text).arrange(RIGHT, buff = 2).to_edge(UP)
-        c = VGroup(b,a).arrange(DOWN, buff = 1)
 
         self.play(Write(title), Write(sum_riemann_text))
-        self.add(a)
-
-        i = 1
-
-        n = PI/0.4
+        self.play(FadeIn(a), FadeIn(origin_label), FadeIn(pi_label))
 
         rects_initial = ax.get_riemann_rectangles(
             graph,
             x_range = [0, PI],
-            dx = 0.4,
+            dx = dx,
             color = ORANGE
         )
 
         self.play(Create(rects_initial), run_time = 1.5)
 
+        sum_riemann_text.add_updater(
+            lambda x: x.become(MathTex(r"\sum_{i=1}^{",f"{n}", "}", "\sin(x_i)", f"{round(dx,2):.2f}", f"={round(sum_riemann_value,3)}", font_size = 48).to_edge(UP+RIGHT*5))
+        )
 
         while True:
-
+            dx = 0.6-(i*0.05)
+            n = int(np.floor(PI/dx))
             rects_next = ax.get_riemann_rectangles(
                 graph,
                 x_range = [0, PI],
-                dx = 0.5-(i*0.1),
+                dx = dx,
                 color = ORANGE
             )
-            self.play(ReplacementTransform(rects_initial, rects_next), run_time = 1.5)
+            sum_riemann_value = self.calculate_riemann_sum(lambda x: np.sin(x), 0, PI, dx)
+            self.play(ReplacementTransform(rects_initial, rects_next), run_time = 1)
             i += 1
             rects_initial = rects_next
-            if i > 4:
+            if i > 11:
                 break
 
         
