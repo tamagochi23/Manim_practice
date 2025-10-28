ee1d2c107687d4e244286943f2fcf8e1782fe803
DavidMonsalves98 <david.tamagochi23@hotmail.com>
2025-10-28 20:05:40 -0300

    another parametric function

--- Tutorial/ParametricGraphs/intro_parametric_graphs.py
+++ Tutorial/ParametricGraphs/intro_parametric_graphs.py
@@ -257,3 +257,57 @@ class ParametricGraphUpdater_4(Scene):
         # self.play(a.animate.set_value(4),run_time=8)
         self.play(b.animate.set_value(4),run_time=8)
         self.wait()
+
+class ParametricGraphUpdater_5(Scene):
+    def construct(self):
+        axes = Axes(
+            x_range=[-3, 3],
+            y_range=[-2, 2],
+            x_length=6,
+            y_length=4,
+            axis_config={"color": BLUE},
+            tips=False,
+        ).scale(1.5)
+        
+        a = ValueTracker(1)
+        b = ValueTracker(1)
+
+        p = lambda t: np.array([
+                np.sin(a.get_value()*(t**3)),
+                np.cos(b.get_value()*(t**3)),
+                0
+            ])
+
+        parametric_curve = ParametricFunction(
+            p,
+            t_range=[-PI, PI],
+            color=RED
+        ).scale(1.5)
+        
+        parametric_curve.add_updater(
+            lambda m: m.become(
+                ParametricFunction(
+                    p,
+                    t_range=[-PI, PI],
+                    color=RED
+                ).scale(1.5)
+            )
+        )
+
+        parametric_text = MathTex(r"f(t) = \sin(","a",r"t^3), \cos(", "b", r"t^3) )", font_size=50).move_to(3.5*UP)
+
+        parametric_text[1].set_color(PURPLE_A)
+        parametric_text[3].set_color(GREEN)
+
+        a_text = MathTex("a = 1.00", font_size=50).move_to(2.7*UP + 3.7*LEFT).set_color(PURPLE_A)  
+        b_text = MathTex("b = 1.00", font_size=50).move_to(2.7*UP + 3.7*RIGHT).set_color(GREEN)
+
+        a_text.add_updater(lambda m: m.become(MathTex(f"a = {a.get_value():.2f}", font_size=50).move_to(2.5*UP + 3.7*LEFT).set_color(PURPLE_A)))
+        b_text.add_updater(lambda m: m.become(MathTex(f"b = {b.get_value():.2f}", font_size=50).move_to(2.5*UP + 3.7*RIGHT).set_color(GREEN)))
+
+        self.add(axes, a_text, b_text, parametric_text) 
+        self.play(Create(parametric_curve), run_time=2)
+        self.wait(1)
+        self.play(a.animate.set_value(2), run_time=12)
+        self.play(b.animate.set_value(2),run_time=12)
+        self.wait()
