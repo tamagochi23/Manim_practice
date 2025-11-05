bf9da92037ef1e39ae5dd081ef1431f839ce3dd5
DavidMonsalves98 <david.tamagochi23@hotmail.com>
2025-11-04 21:47:04 -0300

    adjust first polar plane video

--- Tutorial/PolarPlane/polar_plane_intro.py
+++ Tutorial/PolarPlane/polar_plane_intro.py
@@ -2,23 +2,29 @@ from manim import *
 
 class PolarPlaneIntro(Scene):
     def construct(self):
-        polar_plane = PolarPlane(size=6, azimuth_units="PI radians").set_color(BLUE_E).move_to(DOWN*0.5)
+        polar_plane = PolarPlane(size=6, azimuth_units="PI radians").set_color(BLUE_E).move_to(RIGHT*2+DOWN*0.5)
         a = ValueTracker(1)
         r = lambda theta: 3*np.sin(a.get_value()*theta)
         graph = polar_plane.plot_polar_graph(r, [0, 2*PI],color=RED_E)
-        polar_eq = MathTex(r"r = 3\sin(", "a", r"\theta)", font_size=50).move_to(3.5*UP)
+
+        title = Text("Polar Coordinates", font_size=40, font = "Castellar").move_to(3.5*UP).set_color_by_gradient(BLUE_C, GREEN_C)
+
+        polar_eq = MathTex(r"r = 3\sin(", "a", r"\theta)", font_size=60).move_to(4.0*LEFT + 1*UP)
         polar_eq[1].set_color(PURPLE_A)
 
-        a_text = MathTex("a = 1.00", font_size=50).move_to(2.7*UP + 3.7*LEFT).set_color(PURPLE_A)  
+        a_text = MathTex("a = 1.00", font_size=60).move_to(4.0*LEFT).set_color(PURPLE_A)  
 
         graph.add_updater(
             lambda m: m.become(polar_plane.plot_polar_graph(r, [0, 2*PI], color=RED_E))
         )
 
-        a_text.add_updater(lambda m: m.become(MathTex(f"a = {a.get_value():.2f}", font_size=50).move_to(2.5*UP + 3.7*LEFT).set_color(PURPLE_A)))
+        a_text.add_updater(lambda m: m.become(MathTex(f"a = {a.get_value():.2f}", font_size=60).move_to(4.0*LEFT).set_color(PURPLE_A)))
 
-        self.add(polar_plane, graph, polar_eq, a_text)
-        self.play(a.animate.set_value(3), run_time=6)
+        logo = ImageMobject("../../templates/media/images/logo_pi_x_bit.jpg")
+
+        self.add(title, polar_plane, graph, polar_eq, a_text)
+        self.play(FadeIn(logo.scale(0.27).to_corner(DR)))
+        self.play(a.animate.set_value(5), run_time=13)
         self.wait()
 
 class PolarPlane_2(Scene):
