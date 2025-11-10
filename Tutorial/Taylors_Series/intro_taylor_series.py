7ff8edac8db3b1c4cb03d75900f2dd3d52d4c817
DavidMonsalves98 <david.tamagochi23@hotmail.com>
2025-11-10 19:19:18 -0300

    displaying texts of taylor series of sin(x) video

--- Tutorial/Taylors_Series/intro_taylor_series.py
+++ Tutorial/Taylors_Series/intro_taylor_series.py
@@ -14,45 +14,64 @@ class IntroTaylorSeries(Scene):
         #self.wait(4)
         #self.play(FadeOut(taylor_formula))
         
+        title = Text("Taylor Series", font_size=40, font = "Castellar").move_to(3.5*UP).set_color_by_gradient(BLUE_C, GREEN_C)
 
         axes = Axes(
             x_length=10,
             y_length=6,
             axis_config={"color": GREY},
             tips=False,
-        )
+        ).move_to(DOWN*0.75)
 
-        self.play(Create(axes))
+        taylor_formula = MathTex(
+            "sin(x) ~ x",'- \\frac{x^3}{3!}','+ \\frac{x^5}{5!}','- \\frac{x^7}{7!}','+ \\frac{x^9}{9!}'
+        ).scale(0.8).move_to(1*UP+LEFT*4)
+
+        self.add(title)
+
+        self.add(axes)
 
         # Show an example function and its Taylor series approximation
-        func = FunctionGraph(lambda x: np.sin(x), color=BLUE)
-        taylor_approx_0 = FunctionGraph(
-            lambda x: x , color=RED,
+        func = axes.plot(lambda x: np.sin(x), color=BLUE)
+        taylor_approx_0 = axes.plot(
+            lambda x: x , color=RED, x_range=[-4, 4]
         )
-        taylor_approx_1 = FunctionGraph(
-            lambda x: x - (x**3)/math.factorial(3) , color=RED, 
+        taylor_approx_1 = axes.plot(
+            lambda x: x - (x**3)/math.factorial(3) , color=RED
         )
-        taylor_approx_2 = FunctionGraph(
+        taylor_approx_2 = axes.plot(
             lambda x: x - (x**3)/math.factorial(3) + (x**5)/math.factorial(5), color=RED,
         )
-        taylor_approx_3 = FunctionGraph(
+        taylor_approx_3 = axes.plot(
             lambda x: x - (x**3)/math.factorial(3) + (x**5)/math.factorial(5) - (x**7)/math.factorial(7), 
             color=RED,
         )
-        taylor_approx_4 = FunctionGraph(
+        taylor_approx_4 = axes.plot(
             lambda x: x - (x**3)/math.factorial(3) + (x**5)/math.factorial(5) - (x**7)/math.factorial(7) + (x**9)/math.factorial(9), 
             color=RED,
         )
 
+
         self.play(Create(func), run_time=2)
         self.wait(0.5)
-        self.play(Create(taylor_approx_0), run_time=1)
+
+        self.play(Write(taylor_formula[0]))
+        self.play(Create(taylor_approx_0), run_time=2)
         self.wait(0.5)
-        self.play(Transform(taylor_approx_0, taylor_approx_1), run_time=1)
+
+        self.play(Write(taylor_formula[1]), run_time=0.5)
+        self.play(Transform(taylor_approx_0, taylor_approx_1), run_time=2)
         self.wait(0.5)
-        self.play(Transform(taylor_approx_0, taylor_approx_2), run_time=1)
+
+        self.play(Write(taylor_formula[2]), run_time=0.5)
+        self.play(Transform(taylor_approx_0, taylor_approx_2), run_time=2)
         self.wait(0.5)
-        self.play(Transform(taylor_approx_0, taylor_approx_3), run_time=1)
+
+        self.play(Write(taylor_formula[3]), run_time=0.5)
+        self.play(Transform(taylor_approx_0, taylor_approx_3), run_time=2)
         self.wait(0.5)
-        self.play(Transform(taylor_approx_0, taylor_approx_4), run_time=1)
+
+        self.play(Write(taylor_formula[4]), run_time=0.5)
+        self.play(Transform(taylor_approx_0, taylor_approx_4), run_time=2)
+
         self.wait(2)
