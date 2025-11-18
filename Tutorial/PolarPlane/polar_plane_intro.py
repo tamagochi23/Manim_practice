8c3e51755ca74f211009c343c01f55d6b2558515
DavidMonsalves98 <david.tamagochi23@hotmail.com>
2025-11-17 23:34:33 -0300

    fix some values

--- Tutorial/PolarPlane/polar_plane_intro.py
+++ Tutorial/PolarPlane/polar_plane_intro.py
@@ -36,16 +36,16 @@ class PolarPlane_2(Scene):
 
         title = Text("Polar Coordinates", font_size=40, font = "Castellar").move_to(3.5*UP).set_color_by_gradient(BLUE_C, GREEN_C)
 
-        polar_eq = MathTex(r"r = 3\sin(", "a", r"2\theta)", font_size=60).move_to(4.0*LEFT + 1*UP)
+        polar_eq = MathTex(r"r = 3\sin(\frac{\theta}{", "a", r"})", font_size=60).move_to(4.0*LEFT + 1*UP)
         polar_eq[1].set_color(PURPLE_A)
 
-        a_text = MathTex("a = 1.00", font_size=60).move_to(4.0*LEFT).set_color(PURPLE_A)  
+        a_text = MathTex("a = 1.00", font_size=60).move_to(4.0*LEFT + 0.5*DOWN).set_color(PURPLE_A)  
 
         graph.add_updater(
             lambda m: m.become(polar_plane.plot_polar_graph(r, [0, 2*PI], color=RED_E))
         )
 
-        a_text.add_updater(lambda m: m.become(MathTex(f"a = {a.get_value():.2f}", font_size=60).move_to(4.0*LEFT).set_color(PURPLE_A)))
+        a_text.add_updater(lambda m: m.become(MathTex(f"a = {a.get_value():.2f}", font_size=60).move_to(4.0*LEFT + 0.5*DOWN).set_color(PURPLE_A)))
 
         
         logo = ImageMobject("../../templates/media/images/logo_pi_x_bit.jpg")
