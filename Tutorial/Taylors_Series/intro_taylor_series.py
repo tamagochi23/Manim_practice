767aa9d09d8d426c85a6438ed237dbf781d34205
tamagochi23 <david.tamagochi23@hotmail.com>
2026-04-03 18:47:39 -0300

    fix ln taylor series, add another taylor series function

--- Tutorial/Taylors_Series/intro_taylor_series.py
+++ Tutorial/Taylors_Series/intro_taylor_series.py
@@ -159,15 +159,15 @@ class IntroTaylorSeries_3(Scene):
         taylor_definition = MathTex("f(x) = \\sum_{n=0}^{\\infty} \\frac{f^{(n)}(1)}{n!} (x-1)^n").scale(0.8).move_to(2.5*UP+LEFT*4)
 
         taylor_formula = MathTex(
-            "ln(x) \\approx (x-1)",'- \\frac{(x-1)^2}{2!}','+ \\frac{(x-1)^3}{3!}','- \\frac{(x-1)^4}{4!}','+ \\frac{(x-1)^5}{5!}'
-        ).scale(0.8).move_to(1*UP+LEFT*4)
+            "\\ln(x) \\approx (x-1)",'- \\frac{(x-1)^2}{2!}','+ \\frac{(x-1)^3}{3!}','- \\frac{(x-1)^4}{4!}','+ \\frac{(x-1)^5}{5!}'
+        ).scale(0.52).move_to(1*UP+LEFT*3.5)
 
         self.add(title)
 
         self.add(axes, taylor_definition)
 
         # Show an example function and its Taylor series approximation
-        func = axes.plot(lambda x: np.log(x), color=BLUE, x_range=[0.1, 4])
+        func = axes.plot(lambda x: np.log(x), color=BLUE, x_range=[0.01, 10000])
         taylor_approx_0 = axes.plot(
             lambda x: (x-1) , color=RED
         )
@@ -203,5 +203,68 @@ class IntroTaylorSeries_3(Scene):
         self.wait(0.5)
 
         self.play(Write(taylor_formula[4]),Transform(taylor_approx_0, taylor_approx_4), run_time=2)
+
+        self.wait(2)
+
+class IntroTaylorSeries_4(Scene):
+    def construct(self):
+        
+        title = Text("Taylor Series", font_size=40, font = "Castellar").move_to(3.5*UP).set_color_by_gradient(BLUE_C, GREEN_C)
+
+        axes = Axes(
+            x_length=11,
+            y_length=6,
+            axis_config={"color": GREY},
+            tips=False,
+        ).move_to(DOWN*0.75)
+
+        taylor_definition = MathTex("f(x) = \\sum_{n=0}^{\\infty} \\frac{f^{(n)}(1)}{n!} (x-1)^n").scale(0.8).move_to(2.5*UP+LEFT*4)
+
+        taylor_formula = MathTex(
+            "\\ln(x) \\approx (x-1)",'- \\frac{(x-1)^2}{2!}','+ \\frac{(x-1)^3}{3!}','- \\frac{(x-1)^4}{4!}','+ \\frac{(x-1)^5}{5!}'
+        ).scale(0.52).move_to(1*UP+LEFT*3.5)
+
+        self.add(title)
+
+        self.add(axes, taylor_definition)
+
+        # Show an example function and its Taylor series approximation
+        func = axes.plot(lambda x: 2*np.exp(-1/(x**2)) - 1
+                         , color=BLUE)
+        # taylor_approx_0 = axes.plot(
+        #     lambda x: (x-1) , color=RED
+        # )
+        # taylor_approx_1 = axes.plot(
+        #     lambda x: (x-1) - ((x-1)**2)/math.factorial(2), color=RED
+        # )
+        # taylor_approx_2 = axes.plot(
+        #     lambda x: (x-1) - ((x-1)**2)/math.factorial(2) + ((x-1)**3)/math.factorial(3), color=RED,
+        # )
+        # taylor_approx_3 = axes.plot(
+        #     lambda x: (x-1) - ((x-1)**2)/math.factorial(2) + ((x-1)**3)/math.factorial(3) - ((x-1)**4)/math.factorial(4), 
+        #     color=RED,
+        # )
+        # taylor_approx_4 = axes.plot(
+        #     lambda x: (x-1) - ((x-1)**2)/math.factorial(2) + ((x-1)**3)/math.factorial(3) - ((x-1)**4)/math.factorial(4) + ((x-1)**5)/math.factorial(5), 
+        #     color=RED,
+        # )
+
+
+        self.play(Create(func), run_time=2)
+        self.wait(0.5)
+
+        # self.play(Write(taylor_formula[0]),Create(taylor_approx_0), run_time=2)
+        # self.wait(0.5)
+
+        # self.play(Write(taylor_formula[1]),Transform(taylor_approx_0, taylor_approx_1), run_time=2)
+        # self.wait(0.5)
+
+        # self.play(Write(taylor_formula[2]),Transform(taylor_approx_0, taylor_approx_2), run_time=2)
+        # self.wait(0.5)
+
+        # self.play(Write(taylor_formula[3]),Transform(taylor_approx_0, taylor_approx_3), run_time=2)
+        # self.wait(0.5)
+
+        # self.play(Write(taylor_formula[4]),Transform(taylor_approx_0, taylor_approx_4), run_time=2)
 
         self.wait(2)
